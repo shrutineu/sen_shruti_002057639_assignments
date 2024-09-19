0f4d8a7e5771dddff1cb7e599e9207dd2399cce9
shrutineu <sen.shru@northeastern.edu>
2024-09-19 15:25:53 -0400

    initial commit

---
+++ NetBeansProjects/Assignment1/src/model/Product.java
@@ -0,0 +1,195 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author shrutisen
+ */
+public class Product {
+    String FirstName;
+    String LastName;
+    String Email; 
+    String Phone;
+    String DriversLicenseNumber;
+    String DriversLicenseState;
+    String SSN;
+    String Address1;
+    String Address2;
+    String City;
+    String State;
+    String Zip;
+    String Telephone;
+    String Fax;
+    String DateOfBirth;
+    String Passport;
+    String Occupation;
+    String MaritalStatus;
+    String Nationality;
+    String EmergencyContact;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getDriversLicenseNumber() {
+        return DriversLicenseNumber;
+    }
+
+    public void setDriversLicenseNumber(String DriversLicenseNumber) {
+        this.DriversLicenseNumber = DriversLicenseNumber;
+    }
+
+    public String getDriversLicenseState() {
+        return DriversLicenseState;
+    }
+
+    public void setDriversLicenseState(String DriversLicenseState) {
+        this.DriversLicenseState = DriversLicenseState;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getAddress1() {
+        return Address1;
+    }
+
+    public void setAddress1(String Address1) {
+        this.Address1 = Address1;
+    }
+
+    public String getAddress2() {
+        return Address2;
+    }
+
+    public void setAddress2(String Address2) {
+        this.Address2 = Address2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZip() {
+        return Zip;
+    }
+
+    public void setZip(String Zip) {
+        this.Zip = Zip;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String Telephone) {
+        this.Telephone = Telephone;
+    }
+
+    public String getFax() {
+        return Fax;
+    }
+
+    public void setFax(String Fax) {
+        this.Fax = Fax;
+    }
+
+    public String getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public void setDateOfBirth(String DateOfBirth) {
+        this.DateOfBirth = DateOfBirth;
+    }
+
+    public String getPassport() {
+        return Passport;
+    }
+
+    public void setPassport(String Passport) {
+        this.Passport = Passport;
+    }
+
+    public String getOccupation() {
+        return Occupation;
+    }
+
+    public void setOccupation(String Occupation) {
+        this.Occupation = Occupation;
+    }
+
+    public String getMaritalStatus() {
+        return MaritalStatus;
+    }
+
+    public void setMaritalStatus(String MaritalStatus) {
+        this.MaritalStatus = MaritalStatus;
+    }
+
+    public String getNationality() {
+        return Nationality;
+    }
+
+    public void setNationality(String Nationality) {
+        this.Nationality = Nationality;
+    }
+
+    public String getEmergencyContact() {
+        return EmergencyContact;
+    }
+
+    public void setEmergencyContact(String EmergencyContact) {
+        this.EmergencyContact = EmergencyContact;
+    }
+    
+    
+    
+}
