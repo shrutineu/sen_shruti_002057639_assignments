d749cf019acb6a3fb38c22498fd0ba784cb3d41b
shrutineu <sen.shru@northeastern.edu>
2024-09-28 16:20:20 -0400

    first commit

---
+++ NetBeansProjects/labs/Lab3/src/model/accountdirectory.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author shrutisen
+ */
+public class accountdirectory {
+
+    private ArrayList<account> accounts;
+
+    public accountdirectory() {
+        this.accounts = new ArrayList<account>();
+    }
+
+    public ArrayList<account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(ArrayList<account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public account addAccount() {
+        account a = new account();
+        accounts.add(a);
+        return a;
+    }
+
+    public void deleteAccount(account account) {
+        accounts.remove(account);
+    }
+}
+
